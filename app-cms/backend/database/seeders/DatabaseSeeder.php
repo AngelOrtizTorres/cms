bf1d4444d6672eb0fdcad9969a003424888b6e18
unknown <yestudiouco@gmail.com>
2026-04-22 14:43:52 +0200

    Updating security and implementing argon2id

--- app-cms/backend/database/seeders/DatabaseSeeder.php
+++ app-cms/backend/database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Usuario Prueba',
             'email' => 'usuario@example.com',
-            'password' => bcrypt('contraseña'),
+            'password' => 'contraseña',
             'role' => 'admin',
         ]);
 
